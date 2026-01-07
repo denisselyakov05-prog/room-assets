e65c096de2691c71431d136cd53d1ceb952fd81b
denisselyakov05-prog <denis.selyakov.05@gmail.com>
2026-01-08 00:22:36 +0300

    Исправление ошибок по F12

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    sourcemap: false,
-  },
   server: {
     host: true,        // эквивалент "0.0.0.0"
     port: 5173,        // порт по умолчанию
